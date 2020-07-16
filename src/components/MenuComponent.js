c54773e1405de9baca35acd40aa0368217afd16a
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-07-16 22:04:20 +0600

    End of part 1 React components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -13,8 +13,7 @@ class Menu extends Component {
           category: 'mains',
           label: 'Hot',
           price: '4.99',
-          description:
-            'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+          description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
         },
         {
           id: 1,
@@ -23,8 +22,7 @@ class Menu extends Component {
           category: 'appetizer',
           label: '',
           price: '1.99',
-          description:
-            'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+          description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
         },
         {
           id: 2,
@@ -33,8 +31,7 @@ class Menu extends Component {
           category: 'appetizer',
           label: 'New',
           price: '1.99',
-          description:
-            'A quintessential ConFusion experience, is it a vada or is it a donut?',
+          description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
         },
         {
           id: 3,
@@ -43,21 +40,21 @@ class Menu extends Component {
           category: 'dessert',
           label: '',
           price: '2.99',
-          description:
-            'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-        },
+          description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
       ],
     };
   }
+
   render() {
     const menu = this.state.dishes.map((dish) => {
       return (
-        <div key={dish.id} className='col-12 mt-5'>
-          <Media tag='li'>
+        <div key={dish.id} className="col-12 mt-5">
+          <Media tag="li">
             <Media left middle>
-              <Media object src={dish.image} alt={dish.name}></Media>
+              <Media object src={dish.image} alt={dish.name} />
             </Media>
-            <Media body className='ml-5'>
+            <Media body className="ml-5">
               <Media heading>{dish.name}</Media>
               <p>{dish.description}</p>
             </Media>
@@ -65,10 +62,13 @@ class Menu extends Component {
         </div>
       );
     });
+
     return (
-      <div className='container'>
-        <div className='row'>
-          <Media list>{menu}</Media>
+      <div className="container">
+        <div className="row">
+          <Media list>
+            {menu}
+          </Media>
         </div>
       </div>
     );
